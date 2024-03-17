f7f84649aff5bea85b95ea22497bbcec9d295ad2
Evintkoo <99308658+Evintkoo@users.noreply.github.com>
2024-03-16 23:57:56 -0700

    add some menus and update method

--- api/telegram_bot.py
+++ api/telegram_bot.py
@@ -2,6 +2,8 @@ import telebot
 from genta import GentaAPI
 import os
 from dotenv import load_dotenv, dotenv_values 
+import requests
+import time
 # loading variables from .env file
 load_dotenv() 
 
@@ -22,7 +24,7 @@ def run_telegram_bot(telegram_token:str, prompt:str, genta_token:str, model_name
     @bot.message_handler(commands=['start']) # Start the message
     def send_welcome(message):
         bot.reply_to(message, "Hi there, I am a simple Telegram bot. How can I help you?")
-        chats.append({"role": "user", "content": "Hi there, I am a simple Telegram bot. How can I help you?"})
+        chats.append({"role": "assistant", "content": "Hi there, I am a simple Telegram bot. How can I help you?"})
 
     @bot.message_handler(commands=['help']) # give help to the user
     def send_help(message):
@@ -42,17 +44,31 @@ def run_telegram_bot(telegram_token:str, prompt:str, genta_token:str, model_name
 
     @bot.message_handler(func=lambda message: True)  # Respond to all other messages
     def echo_all(message):
-        try:
-            user_response = {"role": "user", "content": message.text}
-            chats.append(user_response)
-            response = GENTA_API.ChatCompletion(chats,model_name=model_name)
-            response = GENTA_API.ChatCompletion(chat_history=chats, model_name=model_name)
-            response_txt = response[0][0][0]['generated_text']
-            chat = {"role": "assistant", "content": response_txt}
-            chats.append(chat)
-            bot.reply_to(message, response_txt)
-        except:
-            bot.reply_to(message, "sorry Genta API is not available for now")
+        while (True):
+            try:
+                user_response = {"role": "user", "content": message.text}
+                chats.append(user_response)
+                print(chats)
+                response = GENTA_API.ChatCompletion(chat_history=chats,model_name=model_name)
+                response_txt = response[0][0][0]['generated_text']
+                chat = {"role": "assistant", "content": response_txt}
+                chats.append(chat)
+                bot.reply_to(message, response_txt)
+                break
+            except Exception as e:
+                print(e)
+                bot.reply_to(message, "waiting for Model to scale up")
+                continue
+        #try:
+        #    user_response = {"role": "user", "content": message.text}
+        #    chats.append(user_response)
+        #    response = GENTA_API.ChatCompletion(chats,model_name=model_name)
+        #    response_txt = response[0][0][0]['generated_text']
+        #    chat = {"role": "assistant", "content": response_txt}
+        #    chats.append(chat)
+        #    bot.reply_to(message, response_txt)
+        #except:
+        #    bot.reply_to(message, "sorry Genta API is not available for now")
 
     bot.infinity_polling()
     
