3c8c79d0f0f611e90dfced3b0a5f30c3746c8376
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2024-03-21 18:39:42 -0700

    Update telegram_bot.py

--- api/telegram_bot.py
+++ api/telegram_bot.py
@@ -4,8 +4,30 @@ import os
 from dotenv import load_dotenv, dotenv_values 
 import requests
 import time
+
+import requests
+import json
+
 # loading variables from .env file
 load_dotenv() 
+
+ENDPOINT = 'https://4dh8jbxq2gbxjs-8000.proxy.runpod.net/complete'
+HEADERS = {
+    'accept': 'application/json',
+    'Content-Type': 'application/json'
+}
+data = {
+    "model_id": "72c7e262083b192e47449eb203dbe5aa5e6e3b7e4b492a0b8f81f761269d3332",
+    "completion_parameters": {
+        "prompt": [],
+        "max_new_tokens": 2048,
+        "min_length": 8,
+        "temperature": 0.7,
+        "top_p": 0,
+        "random_seed": 0,
+        "streaming": False
+    }
+}
 
 
 def run_telegram_bot(telegram_token:str, prompt:str, genta_token:str, model_name:str):
@@ -18,7 +40,6 @@ def run_telegram_bot(telegram_token:str, prompt:str, genta_token:str, model_name
         model_name (str): name of model
     """
     # Replace with your actual token 
-    GENTA_API = GentaAPI(token=genta_token)
     bot = telebot.TeleBot(telegram_token)
     chats = [{"role": "system", "content":prompt}]
     @bot.message_handler(commands=['start']) # Start the message
@@ -49,8 +70,9 @@ def run_telegram_bot(telegram_token:str, prompt:str, genta_token:str, model_name
                 user_response = {"role": "user", "content": message.text}
                 chats.append(user_response)
                 print(chats)
-                response = GENTA_API.ChatCompletion(chat_history=chats,model_name=model_name)
-                response_txt = response[0][0][0]['generated_text']
+                data['completion_parameters']['prompt'] = chats
+                response = requests.post(url, headers=headers, data=json.dumps(data))
+                response_txt = response['text']
                 chat = {"role": "assistant", "content": response_txt}
                 chats.append(chat)
                 bot.reply_to(message, response_txt)
@@ -71,6 +93,8 @@ def run_telegram_bot(telegram_token:str, prompt:str, genta_token:str, model_name
         #    bot.reply_to(message, "sorry Genta API is not available for now")
 
     bot.infinity_polling()
+
+
     
 if __name__ == "__main__":
     run_telegram_bot(telegram_token= os.getenv('CHATBOT_TOKEN'),
